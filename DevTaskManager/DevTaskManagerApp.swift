94d69e1fe260de3ed6a3995c8340b1feb9299af3
lburris57 <lburris@mac.com>
2025-04-12 00:45:58 -0400

    Initial Commit

---
+++ DevTaskManager/DevTaskManagerApp.swift
@@ -0,0 +1,17 @@
+//
+//  DevTaskManagerApp.swift
+//  DevTaskManager
+//
+//  Created by Larry Burris on 4/12/25.
+//
+
+import SwiftUI
+
+@main
+struct DevTaskManagerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ DevTaskManagerTests/DevTaskManagerTests.swift
@@ -0,0 +1,17 @@
+//
+//  DevTaskManagerTests.swift
+//  DevTaskManagerTests
+//
+//  Created by Larry Burris on 4/12/25.
+//
+
+import Testing
+@testable import DevTaskManager
+
+struct DevTaskManagerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
